77507d5514a08411ff59dfedbd1450cfdb78102e
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-01-20 18:55:42 +0500

    Integrated Tailwind in our project

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const hbs = require("hbs");
 const path = require("path");
-
 const PORT = 8080;
 const app = express();
+const staticPath = path.join(__dirname, "public");
+
+// Set static path
+app.use(express.static(staticPath));
+
+// Set templating engine
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/src/views");
 

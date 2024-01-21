0998235fdcac5b97779b3fd2c094bf5f0132d57f
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-01-21 10:59:36 +0500

    Created partials
    Different files for different sections

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const path = require("path");
 const PORT = 8080;
 const app = express();
 const staticPath = path.join(__dirname, "../public");
+const partialsPath = path.join(__dirname, "/views/partials");
 
 // Set static path
 app.use(express.static(staticPath));
@@ -12,8 +13,11 @@ app.use(express.static(staticPath));
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 
+// Set path for partials for handlebars
+hbs.registerPartials(partialsPath);
+
 app.get("/", (req, res) => {
-    res.render("demo");
+    res.render("index");
 });
 
 app.listen(PORT, () => console.log("Server running on port " + PORT));

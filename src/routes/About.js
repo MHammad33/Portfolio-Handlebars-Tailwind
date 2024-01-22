6cdcfe2bdbdbfc2d00771fd2e9ff12184f5bcaee
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-01-22 12:42:15 +0500

    Data Handling in Backend for About Section

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const hbs = require("hbs");
+
+const skills = [
+    { skill: "HTML", proficiency: "3/4" },
+    { skill: "JavaScript", proficiency: "4/5" },
+    { skill: "React", proficiency: "5/6" },
+];
+
+let aboutDescription = "I design and build digital products. I'm also a multi-disciplinary maker with over 10 years of experiences in wide range of design disciplines.";
+
 
 router.get("/", (req, res) => {
-    res.render("about");
+    res.render("about", {
+        aboutDescription, skills
+    });
 });
 
 module.exports = router;

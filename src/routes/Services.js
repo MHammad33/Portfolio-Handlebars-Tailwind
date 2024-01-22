ec18f17cea6a93d81ee76d7cfd116cf47398c1a6
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-01-22 12:50:46 +0500

    Data Handling in Backend for Services Section

--- src/routes/Services.js
+++ src/routes/Services.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
+const servicesDescription = "My approach to website design is to create a website that strengthens your company’s brand while ensuring ease of use and simplicity for your audience."
+
+const serviceCards = [
+    {
+        name: "React development", logoPath: "/img/icon.png", desc: "I specialize in creating interactive websites for individuals and small businesses."
+    },
+    {
+        name: "Web development", logoPath: "/img/icon.png", desc: "I specialize in creating interactive websites for individuals and small businesses."
+    },
+    {
+        name: "React development", logoPath: "/img/icon.png", desc: "I specialize in creating interactive websites for individuals and small businesses."
+    },
+];
+
 router.get("/", (req, res) => {
-    res.render("services");
+    res.render("services", { servicesDescription, serviceCards });
 });
 
 module.exports = router;

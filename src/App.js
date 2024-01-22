859968576574258c998120af23534e85c56eabd7
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-01-22 11:30:43 +0500

    Routes Setup for all pages

--- src/App.js
+++ src/App.js
@@ -11,11 +11,21 @@ app.use(express.static(staticPath));
 
 // Set templating engine
 app.set("view engine", "hbs");
-app.set("views", __dirname + "/views");
+app.set("views", path.join(__dirname, "views"));
 app.set("view options", { layout: "layouts/main" });
 
 // Set path for partials for handlebars
 hbs.registerPartials(partialsPath);
+
+// Setting Routes
+const aboutRoute = require("./routes/About");
+const workRoute = require("./routes/Work");
+const contactRoute = require("./routes/Contact");
+const servicesRoute = require("./routes/Services");
+app.use("/about", aboutRoute);
+app.use("/work", workRoute);
+app.use("/contact", contactRoute);
+app.use("/services", servicesRoute);
 
 app.get("/", (req, res) => {
     res.render("index");

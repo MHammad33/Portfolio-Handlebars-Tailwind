1a74b234abf169bdfffdafda4aca158d77734b4f
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-01-22 13:05:22 +0500

    Data Handling in Backend for Contact Section

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+const contactDesc = "Do you have an idea? Let's discuss it and see what we can do together.";
+
+const contactInputs = [
+    { type: "text", placeholder: "Name Surname" },
+    { type: "email", placeholder: "Email" },
+];
+
 router.get("/", (req, res) => {
-    res.render("contact");
+    res.render("contact", { contactDesc, contactInputs });
 });
 
 module.exports = router;

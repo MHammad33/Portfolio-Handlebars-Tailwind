6b2f2f8db72835c7bbb99ffcef705e01b1755fed
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-12 16:12:32 +0500

    Use cors for connection with Frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
 const app = express();
+const cors = require("cors");
+
+// Middlewares
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");

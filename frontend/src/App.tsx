2b0724aecdc6bdf6c68401c031003d06c9d98e93
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-12 15:39:59 +0500

    Tailwind Setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-	return <>Hello, Portfolio</>;
+	return (
+		<h1 className="text-3xl font-bold underline text-amber-700">
+			Hello, Portfolio
+		</h1>
+	);
 }
 
 export default App;

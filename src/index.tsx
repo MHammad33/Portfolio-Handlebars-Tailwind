3a9c203e7f6a3dfae26f2daea02f2e8319f96aec
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-08-04 06:11:52 -0400

    Create Layout with React Router DOM

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Admin from "./components/admin/Admin";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Layout />,
+		children: [
+			{ path: "/", element: <App /> },
+			{ path: "/admin", element: <Admin /> },
+		],
+	},
+]);
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
